18921abac14fef7fab133bce081cb5f12776a601
Rickhersd <Rickhersd2002@gmail.com>
2022-11-27 21:52:29 -0400

    Create DB with Fake Data

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //One to Many Inverse Relationship
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //One-To-Many Relationship
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //polymorphic one-to-many relationship
+
+    public function image(){
+        return $this->morphOne(image::class, 'imageable');
+    }
 }
--- database/migrations/2022_11_27_002444_create_posts_table.php
+++ database/migrations/2022_11_27_002444_create_posts_table.php
@@ -25,7 +25,7 @@ class CreatePostsTable extends Migration
             $table->unsignedBigInteger('category_id');
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $tables->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
